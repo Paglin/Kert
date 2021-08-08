14f36968847fe8c1c1cbdd12d7be82227d60012f
Paglin <88614432+Paglin@users.noreply.github.com>
2021-08-08 04:08:59 -0700

    Cookies

---
+++ index.js
@@ -0,0 +1,26 @@
+let x = document.cookie;
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return null;
+}
+
+if (getCookie("DId") == null){
+    window.alert('Uh Oh');
+    var cname = "DId"
+    var exdays = 3650
+    const d = new Date
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + ";" + expires + ";path=/";
+};
